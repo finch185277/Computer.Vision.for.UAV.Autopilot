309e331cd527f0095b532069638e7a176fdddde4
finch185277 <31809727+finch185277@users.noreply.github.com>
2020-03-20 11:47:48 +0800

    Update lab1

--- lab1/main.py
+++ lab1/main.py
@@ -1,16 +1,18 @@
 import cv2
 import numpy as np
 
+
 def rgb_to_grey(img):
     height, width, depth = img.shape
     ret = np.zeros((height, width, 1), np.uint8)
     for x in range(width):
         for y in range(height):
-            grey_value = 0;
+            grey_value = 0
             for z in range(depth):
                 grey_value += img[y, x, z]
             ret[y, x, 0] = grey_value / 3
     return ret
+
 
 def nearest_neighbor_interpolation(img, r):
     ret = np.zeros((int(img.shape[0] * r), int(img.shape[1] * r), img.shape[2]), np.uint8)
@@ -20,6 +22,7 @@ def nearest_neighbor_interpolation(img, r):
             for z in range(depth):
                 ret[y, x, z] = img[int(y/r), int(x/r), z]
     return ret
+
 
 def bilinear_interpolation(img, r):
     ret = np.zeros((int(img.shape[0] * r), int(img.shape[1] * r), img.shape[2]), np.uint8)
@@ -31,12 +34,17 @@ def bilinear_interpolation(img, r):
                     ret[y, x, z] = img[int(y/r), int(x/r), z]
             else:
                 for z in range(depth):
-                    ret[y, x, z] += img[int(y/r), int(x/r), z] * (int(x/r) + 1 - float(x/r)) * (int(y/r) + 1 - float(y/r))
-                    ret[y, x, z] += img[int(y/r + 1), int(x/r), z] * (int(x/r) + 1 - float(x/r)) * (float(y/r) - int(y/r))
-                    ret[y, x, z] += img[int(y/r), int(x/r + 1), z] * (float(x/r) - int(x/r)) * (int(y/r) + 1 - float(y/r))
-                    ret[y, x, z] += img[int(y/r + 1), int(x/r + 1), z] * (float(x/r) - int(x/r)) * (float(y/r) - int(y/r))
+                    ret[y, x, z] += img[int(y/r), int(x/r), z] * \
+                        (int(x/r) + 1 - float(x/r)) * (int(y/r) + 1 - float(y/r))
+                    ret[y, x, z] += img[int(y/r + 1), int(x/r), z] * \
+                        (int(x/r) + 1 - float(x/r)) * (float(y/r) - int(y/r))
+                    ret[y, x, z] += img[int(y/r), int(x/r + 1), z] *\
+                        (float(x/r) - int(x/r)) * (int(y/r) + 1 - float(y/r))
+                    ret[y, x, z] += img[int(y/r + 1), int(x/r + 1), z] * \
+                        (float(x/r) - int(x/r)) * (float(y/r) - int(y/r))
 
     return ret
+
 
 img1 = cv2.imread('kobe.jpg')
 img2 = cv2.imread('IU.png')
