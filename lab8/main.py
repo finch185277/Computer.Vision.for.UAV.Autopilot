dccbcb826dda47b0b0e17cafc1333583c35fe50c
finch185277 <31809727+finch185277@users.noreply.github.com>
2020-05-20 15:05:27 +0800

    Update lab8
    - add distance detect

--- lab8/main.py
+++ lab8/main.py
@@ -4,6 +4,9 @@ import os
 import glob
 import imutils
 
+def distance_to_camera(knownH, focal_length, perH):
+	# compute and return the distance from the maker to the camera
+	return (knownH * focal_length) / perH
 
 testboard = (5,7)
 
@@ -51,29 +54,50 @@ f.write("distortion",dist)
 dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
 parameters = cv2.aruco.DetectorParameters_create()
 
+focal_length = (mtx[0, 0] + mtx[1, 1]) / 2
+
 cam = cv2.VideoCapture(1)
 while True:
     ret,frame = cam.read()
     frame = imutils.resize(frame, width=min(400, frame.shape[1]))
     if not ret:
         break
-    hog = cv2.HOGDescriptor()
-    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-    rects, weights = hog.detectMultiScale(frame,winStride=(4,4),padding = (8,8),scale=1.03,useMeanshiftGrouping=False)
-    for (x,y,w,h) in rects:
-        frame = cv2.rectangle(frame,(x,y),(x+w,y+h), (0,255,0), 2)
-
-
 
     face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
-    faces = face_cascade.detectMultiScale(frame,scaleFactor=1.2)
+    faces = face_cascade.detectMultiScale(frame, scaleFactor=1.2)
 
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+    rects, weights = hog.detectMultiScale(frame, winStride=(4,4), padding = (8,8), scale=1.03, useMeanshiftGrouping=False)
 
+    face_H = 15
+    rect_W = 80
+    faces_perH = 1
+    rects_perW = 1
 
-    for (x,y,w,h) in rects:
-        frame = cv2.rectangle(frame,(x,y),(x+w,y+h), (0,255,0), 2)
+    # faces
     for (x,y,w,h) in faces:
         frame = cv2.rectangle(frame,(x,y),(x+w,y+h), (255,0,0), 2)
+        faces_perH = h
+
+    # rects
+    mx = 0
+    my = 0
+    mw = 0
+    mh = 0
+    for (x,y,w,h) in rects:
+        if h > mh:
+            mx = x
+            my = y
+            mw = w
+            mh = h
+    frame = cv2.rectangle(frame,(mx,my),(mx+mw,my+mh), (0,255,255), 2)
+    rects_perW = mw
+
+    # faces
+    cv2.putText(frame, '%f' % distance_to_camera(face_H, focal_length, faces_perH), (10,80), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1, cv2.LINE_AA)
+    # rects
+    cv2.putText(frame, '%f' % distance_to_camera(rect_W, focal_length, rects_perW), (10,40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 1, cv2.LINE_AA)
 
     cv2.imshow("Image",frame)
     cv2.waitKey(1)
